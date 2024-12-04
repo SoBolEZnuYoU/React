22361a75573c31a28b4a37eaf03933a85dfc56b1
SoBolEZnuYoU <119670689+SoBolEZnuYoU@users.noreply.github.com>
2024-12-04 13:04:21 +0400

    Changed the code in App.js using JSX to without JSX

--- react-with-create-react-app/src/App.js
+++ react-with-create-react-app/src/App.js
@@ -1,32 +1,38 @@
 import logo from './logo.svg'
+import { createElement } from 'react'
 import './App.css'
 
-/* Как по мне, весь этот код является императивным, так как в любом случае идет описание того, что должна возвращать функция.
-Такой-то блок с определенным классом, внутри блока такие-то элементы, с указанием класса у каждого, и местами такой-то текст.
-В таком случае под декларативный подход подойдёт разве что метод getFullYear(). Но опять же, если сравнивать с созданием такой же конструкции
-на чистом JavaScript, тогда весь этот код можно назвать декларативным*/
-
 function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<p>
-					{(new Date().getFullYear())}
-				</p>
-			</header>
-		</div>
+	return createElement(
+		'div',
+		{ className: 'App' },
+		createElement(
+			'header',
+			{ className: 'App-header' },
+			createElement('img', {
+				src: logo,
+				className: 'App-logo',
+				alt: 'Logo',
+			}),
+			createElement(
+				'p',
+				null,
+				'Edit ',
+				createElement('code', null, 'src/App.js'),
+				' and save to reload',
+			),
+			createElement(
+				'a',
+				{
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer',
+				},
+				'Learn react',
+			),
+			createElement('p', null, new Date().getFullYear()),
+		),
 	)
 }
 
